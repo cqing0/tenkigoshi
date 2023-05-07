e36bd67708213ac492f9900724fab6e372c55444
mauiman77 <boerbasher@protonmail.com>
2023-05-07 15:25:18 +0100

    Install css-loader style-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import './style.css';
+
 const body = document.querySelector('body');
-body.style.cssText = 'height: 100%; background-color: rgb(100,100,100);';
 
 async function testApi() {
   const api = await fetch('http://api.weatherapi.com/v1/forecast.json?key=51e9b20e2c0f4057a77134636230505&q=glasgow');
