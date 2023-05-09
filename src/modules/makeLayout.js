db499228412d59ea07f3140aa580ab52d50fab3b
mauiman77 <boerbasher@protonmail.com>
2023-05-09 13:35:39 +0100

    Add div structure

--- src/modules/makeLayout.js
+++ src/modules/makeLayout.js
@@ -7,27 +7,9 @@ const layout = (() => {
     return document.querySelector('main.mainBit');
   }
 
-  function createMainBit() {
-    const mainBit = document.createElement('main');
-    mainBit.classList.add('mainBit');
-    return mainBit;
-  }
-
   function createSearch() {
     const searchBar = document.createElement('text');
     return searchBar;
-  }
-
-  function createDiv(classAdd) {
-    const div = document.createElement('div');
-    div.classList.add(classAdd);
-    const main = findMain();
-    main.appendChild(div);
-    return div;
-  }
-
-  function createSubDiv() {
-    return 0;
   }
 
   const addText = (text) => {
@@ -38,14 +20,10 @@ const layout = (() => {
   };
 
   const initPage = () => {
-    body.appendChild(createMainBit());
     body.appendChild(createSearch());
-
-    createDiv('location-info');
-    createDiv('weather-info');
   };
 
-  return { initPage, addText, createDiv };
+  return { initPage, addText };
 })();
 
 export default layout;
